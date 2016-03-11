b3a39fa684bd3631b6b9efa9e15a0cd91e8a9f3b
timothysanders <sanders.timothy.j@gmail.com>
2016-03-11 10:40:19 -0800

    initial commit

---
+++ WordCount.py
@@ -0,0 +1,12 @@
+word_string = 'This is a string that Tim has created. This string has many words from Tim in different orders. This should count the number of characters in the string here.'
+
+word_list = word_string.split()
+
+word_dict = {}
+
+for word in word_list:
+  iterWord = word.lower().strip('.')
+  if iterWord not in word_dict:
+    word_dict[iterWord] = 1
+  else:
+    word_dict[iterWord] = word_dict[iterWord] + 1
