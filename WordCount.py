38b14b20938b2f6daa5bfc70b47bd62001155d4f
timothysanders <sanders.timothy.j@gmail.com>
2016-03-11 11:02:23 -0800

    test commit

--- WordCount.py
+++ WordCount.py
@@ -1,3 +1,5 @@
+# comment for change commit
+
 word_string = 'This is a string that Tim has created. This string has many words from Tim in different orders. This should count the number of characters in the string here.'
 
 word_list = word_string.split()
